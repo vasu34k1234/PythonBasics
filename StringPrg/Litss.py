21d07e065ff689a7822a811704d3924e7da898b3
vasu34k1234 <vasu34k@gmail.com>
2018-02-27 17:17:00 +0530

    Add files via upload

---
+++ StringPrg/Litss.py
@@ -0,0 +1,17 @@
+##Lists are like Arrays
+
+lists = [ 'Srini', 'Willis' , 'AIA', 'Scope','coke' ]
+#tinylist = [123, 'Discover']
+
+print (lists)
+print (lists[0])
+print (lists[1:3])
+print (lists[2:])
+# (tinylist * 2)
+#print (lists + tinylist)
+print(lists[2])
+lists[3]='coco'
+print(lists[2+1])
+
+list(enumerate(lists))
+
